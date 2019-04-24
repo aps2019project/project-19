59f95418689d15a1507243d64f53f26af9691906
Amir <a.h.hadian@gmail.com>
2019-04-24 08:58:24 +0430

    Target class added

---
+++ src/model/Target/Target.java
@@ -0,0 +1,19 @@
+package model.Target;
+
+public class Target {
+    private Type type;
+    private int areaSize;
+    private SoldierTargetType soldierTargetType;
+
+    public Type getType() {
+        return type;
+    }
+
+    public int getAreaSize() {
+        return areaSize;
+    }
+
+    public SoldierTargetType getSoldierTargetType() {
+        return soldierTargetType;
+    }
+}
