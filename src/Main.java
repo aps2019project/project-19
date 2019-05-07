87983a104f6fd34bf88bd5aae54531dcd2410038
Amir <a.h.hadian@gmail.com>
2019-05-07 23:59:26 +0430

    json

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,12 @@
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 import controller.CardInitializer;
 import controller.Controller;
-import model.*;
-import model.Buff.*;
-import model.Cards.Hero;
-import model.Cards.Minion;
-import model.Cards.SoldierTypes;
-import model.Cards.SpellCard;
-import model.Target.SoldierTargetType;
-import model.Target.Target;
-import model.Target.Type;
 
-import java.awt.image.AreaAveragingScaleFilter;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Random random = new Random();
-        Gson gson = new GsonBuilder().registerTypeAdapter(Buff.class, new BuffAdapter()).create();
-//        URL url = Main.class.getResource("data/minions.json");
-//        Reader reader = new FileReader(url.getPath());
-//        ArrayList<Minion> readMinions = gson.fromJson(reader, new TypeToken<List<Minion>>(){}.getType());
-//        System.out.println(readMinions.get(0).getName());
         CardInitializer cardInitializer = CardInitializer.getInstance();
         cardInitializer.createCards();
-        //todo: write description
         Controller controller = Controller.getInstance();
         controller.run();
     }
