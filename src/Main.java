87462267dc16ea15d36c5aad38cbe491ce21e4d6
Amir <a.h.hadian@gmail.com>
2019-06-12 13:53:40 +0430

    merge commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import controller.AccountManagement;
 import controller.CardInitializer;
 import controller.Controller;
 import view.Graphic.Graphic;
@@ -8,17 +9,19 @@ import java.net.Socket;
 
 public class Main {
     private Controller controller;
+
     public static void main(String[] args) throws IOException {
         CardInitializer cardInitializer = CardInitializer.getInstance();
         cardInitializer.createCards();
+        AccountManagement.readAccounts();
 //        ServerSocket serverSocket = new ServerSocket(8888);
 //        Socket client = serverSocket.accept();
 
         Controller controller = Controller.getInstance();
-        new Thread(() -> Graphic.main(args,controller.getRequest())).start();
-        try{
+        new Thread(() -> Graphic.main(args, controller.getRequest())).start();
+        try {
             Thread.sleep(500);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         controller.run();
