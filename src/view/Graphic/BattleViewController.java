f67f5b0bd0034489c8ba65f2f9300e8d1772ac1f
sreyhani <sajjad7997@gmail.com>
2019-06-24 16:29:56 +0430

    battle view improved

--- src/view/Graphic/BattleViewController.java
+++ src/view/Graphic/BattleViewController.java
@@ -3,6 +3,7 @@ package view.Graphic;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -10,6 +11,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import model.Game.Game;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,13 +21,17 @@ public class BattleViewController extends MenuController implements Initializabl
     AnchorPane center;
     @FXML
     HBox deckBar;
+//    private Game game = getMainController().getGame();
     //    private final int cellsLength = getMainController().getGame().getLength();
 //    private final int cellsWeight = getMainController().getGame().getWidth();
     //todo: set wight and lenght with game
     private final int cellsLength = 9;
     private final int cellsWeight = 5;
     private AnchorPane[][] anchorPaneCells;
-
+    @FXML
+    private AnchorPane rightHeroAnchor;
+    @FXML
+    private AnchorPane leftHeroAnchor;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         anchorPaneCells = new AnchorPane[cellsWeight][cellsLength];
@@ -46,12 +52,41 @@ public class BattleViewController extends MenuController implements Initializabl
                     anchorPane.getStyleClass().add("cells");
                 });
 
-                Image image = new Image("View/Graphic/cards/arash idle.gif");
+                Image image = new Image("view/Graphic/cards/arash idle.gif");
                 ImageView imageView = new ImageView(image);
                 imageView.setFitWidth(100);
                 imageView.setFitHeight(100);
+                imageView.setLayoutY(-20);
+                imageView.setPreserveRatio(true);
+                imageView.setPickOnBounds(true);
                 anchorPane.getChildren().add(imageView);
+
             }
+        ImageView rightHeroIcon = new ImageView(new Image("view/Graphic/cards/arash.png"));
+        rightHeroIcon.setFitHeight(300);
+        rightHeroIcon.setFitWidth(300);
+        rightHeroIcon.setLayoutY(-50);
+        rightHeroIcon.setPreserveRatio(true);
+        rightHeroIcon.setPickOnBounds(true);
+        rightHeroAnchor.getChildren().add(rightHeroIcon);
+        ImageView leftHeroIcon = new ImageView(new Image("view/Graphic/cards/afsaneh.png"));
+        leftHeroIcon.setFitHeight(300);
+        leftHeroIcon.setFitWidth(300);
+        leftHeroIcon.setPreserveRatio(true);
+        leftHeroIcon.setPickOnBounds(true);
+        leftHeroIcon.setLayoutY(-50);
+        leftHeroAnchor.getChildren().add(leftHeroIcon);
+        for (Node child : deckBar.getChildren()) {
+            Image image = new Image("view/Graphic/cards/arash idle.gif");
+            ImageView imageView = new ImageView(image);
+            imageView.setPreserveRatio(true);
+            imageView.setPickOnBounds(true);
+            imageView.setFitWidth(200);
+            imageView.setFitHeight(200);
+            imageView.setLayoutY(-50);
+
+            ((AnchorPane)child).getChildren().add(imageView);
+        }
 
     }
 }
