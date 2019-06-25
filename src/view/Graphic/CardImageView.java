46fcb6e27e8d057cc2e87347fd0306d878e0ec1c
sreyhani <sajjad7997@gmail.com>
2019-06-25 15:18:54 +0430

    battle view improved

---
+++ src/view/Graphic/CardImageView.java
@@ -0,0 +1,43 @@
+package view.Graphic;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class CardImageView extends ImageView {
+    String cardName;
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public CardImageView(String cardName,Stance stance){
+        super(new Image("view/Graphic/cards/" + cardName + " "+stance.getMessage()+".gif"));
+        this.cardName=cardName;
+    }
+    public void changeStance(Stance newStance){
+        this.setImage(new Image("view/Graphic/cards/" + cardName + " "+newStance.getMessage()+".gif"));
+    }
+    public enum Stance{
+
+        RUNING("run"),
+        ATTACKING("attack"),
+        CASTING("casting"),
+        IDLING("idle"),
+        BREATHING("breathing"),
+        DYING("death");
+        private String stance;
+
+        Stance(String message) {
+            this.stance = message;
+        }
+
+        public String getMessage() {
+            return stance;
+        }
+
+        }
+}
